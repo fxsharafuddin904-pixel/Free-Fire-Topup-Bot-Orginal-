f970db88dcb3b57ef8650f46524ec80485b2f04f
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-27 12:20:59 +0600

    Update database.js

--- database.js
+++ database.js
@@ -1,16 +1,83 @@
 const fs = require("fs");
 
-const DB_FILE = "database.json";
+const DB_FILE = "./packages.json";
 
-function loadDatabase() {
-  if (!fs.existsSync(DB_FILE)) {
-    fs.writeFileSync(DB_FILE, JSON.stringify({ users: {} }, null, 2));
-  }
-  return JSON.parse(fs.readFileSync(DB_FILE, "utf8"));
+function loadPackages() {
+    if (!fs.existsSync(DB_FILE)) {
+        fs.writeFileSync(DB_FILE, "[]");
+    }
+
+    return JSON.parse(
+        fs.readFileSync(DB_FILE, "utf8")
+    );
 }
 
-function saveDatabase(db) {
-  fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2));
+function savePackages(packages) {
+    fs.writeFileSync(
+        DB_FILE,
+        JSON.stringify(packages, null, 2)
+    );
 }
 
-module.exports = { loadDatabase, saveDatabase };
+function getAllPackages() {
+    return loadPackages();
+}
+
+function getPackage(id) {
+    const packages = loadPackages();
+
+    return packages.find(
+        pkg => String(pkg.id) === String(id)
+    );
+}
+
+function addPackage(data) {
+    const packages = loadPackages();
+
+    packages.push(data);
+
+    savePackages(packages);
+}
+
+function updatePackage(id, data) {
+    const packages = loadPackages();
+
+    const index = packages.findIndex(
+        pkg => String(pkg.id) === String(id)
+    );
+
+    if (index === -1) return false;
+
+    packages[index] = {
+        ...packages[index],
+        ...data
+    };
+
+    savePackages(packages);
+
+    return true;
+}
+
+function deletePackage(id) {
+    const packages = loadPackages();
+
+    const filtered = packages.filter(
+        pkg => String(pkg.id) !== String(id)
+    );
+
+    if (filtered.length === packages.length) {
+        return false;
+    }
+
+    savePackages(filtered);
+
+    return true;
+}
+
+module.exports = {
+    getAllPackages,
+    getPackage,
+    addPackage,
+    updatePackage,
+    deletePackage
+};
